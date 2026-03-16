44fbf16d92c136a483c866a1c3511abbdb151979
Brandon Teoh <brandonteohph@gmail.com>
2026-03-16 23:38:05 +0800

    Initial Commit

---
+++ src/main/java/com/foundation/spring_boot_baseline/Application.java
@@ -0,0 +1,13 @@
+package com.foundation.spring_boot_baseline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
